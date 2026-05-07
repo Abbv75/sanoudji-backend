a74420294f87a419508028c86b5d15cb44b81386
abbv75 <bore.younous59@gmail.com>
2026-05-07 11:09:18 +0000

    feat: create metadata_attributes table and corresponding Eloquent model

---
+++ app/Models/MetadataAttribute.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class MetadataAttribute extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'dataType',
+    ];
+}
