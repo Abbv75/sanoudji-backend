4e95fb13b02873cb3357b013f5e39e51f338ab12
abbv75 <bore.younous59@gmail.com>
2026-05-07 11:38:05 +0000

    feat: implement UserSeeder to populate database with sample admin and client accounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RoleSeeder::class);
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $password = Hash::make('password');
+
+        $roles = Role::all()->pluck('id');
+
+        foreach ($roles as $role) {
+            User::create([
+                'id' => fake()->uuid(),
+                'firstName' => fake()->firstName(),
+                'lastName' => fake()->lastName(),
+                'email' => fake()->unique()->safeEmail(),
+                'password' => $password,
+                'phone' => fake()->optional()->phoneNumber(),
+                'profilePhotoUrl' => fake()->optional()->imageUrl(),
+                'id_role' => $role,
+            ]);
+        }
+    }
+}
